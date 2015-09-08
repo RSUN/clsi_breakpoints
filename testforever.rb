99d4adca8989706b60f0be9d87ab1c5661ca6b02
adamlwalker <adamlwalker77@gmail.com>
2015-09-08 03:04:52 -0400

    expanded testforever.rb

--- testforever.rb
+++ testforever.rb
@@ -1,4 +1,7 @@
+puts "Watch log in new window with: tail -f log/test.log"
 while true
     puts "running tests again..."
      `rake test:models`
+    puts "Sleeping for 2 minutes so you can review log"
+    sleep(120) 
 end
